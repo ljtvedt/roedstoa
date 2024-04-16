1a6727c87963b97fbc7bc18a3bb27e3bfb1ef844
Lars Jørgen Tvedt <ljtvedt@gmail.com>
2024-04-16 21:33:56 +0200

    Leser heile dokumentet

--- wp/wp2styreweb/src/Migrate/Program.fs
+++ wp/wp2styreweb/src/Migrate/Program.fs
@@ -1,59 +1,13 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open System
-open System.IO
-open System.Xml.Serialization
 open FSharp.Collections
-
-[<CLIMutable>]
-[<XmlRoot("item")>] 
-type Item = {
-        [<XmlElement("title")>]
-        title: string
-        [<XmlElement("pubDate")>]
-        pubDate:string
-        [<XmlElement("creator", Namespace = "http://purl.org/dc/elements/1.1/")>]
-        creator: string
-        [<XmlElement("post_type", Namespace = "http://wordpress.org/export/1.2/")>]
-        post_type: string
-        [<XmlElement("encoded", Namespace = "http://purl.org/rss/1.0/modules/content/")>]
-        content_encoded: string
-}
-
-[<CLIMutable>]
-[<XmlRoot("channel")>] 
-type Channel = 
-    {
-        [<XmlElement("title")>]
-        title: string
-
-        [<XmlElement("link")>]
-        link: string
-
-        [<XmlElement>]
-        item: Item []
-    }
-
-[<CLIMutable>]
-[<XmlRootAttribute(ElementName="rss")>]
-type rss = 
-    { 
-        [<XmlAttribute("version")>]
-        version: string
-
-        [<XmlElement("channel")>]
-        channel: Channel
-    }
-
+open Migrate
 
 [<EntryPoint>]
 let main args =
-    let serializer = XmlSerializer(typeof<rss>);
-    use reader = new FileStream(@"redstavel.wordpress.2024-01-20.xml", FileMode.Open)
-    let rss = serializer.Deserialize(reader) :?> rss;
-
-    printfn $"{rss.version}"
+    let rss = WpModel.deserialize @"redstavel.wordpress.2024-01-20.xml"
 
     rss.channel.item
-    |> Array.iter (fun x -> printfn $"{x.title}: {x.post_type}: {x.creator}: {x.content_encoded}")
+    |> Array.iter (fun x -> printfn $"{x}")
 
     0
