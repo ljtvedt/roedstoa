e1b0db0f319e5a2273359d592528aab53d4eee8a
Lars Jørgen Tvedt <lars.jorgen.tvedt@nrk.no>
2024-05-10 23:53:22 +0200

    Kalkulerer nye filnamn

--- wp/wp2styreweb/src/Migrate/Program.fs
+++ wp/wp2styreweb/src/Migrate/Program.fs
@@ -4,19 +4,20 @@ open System.Text.RegularExpressions
 open FSharp.Collections
 open Migrate
 open SWModel
+open System.Globalization
+open System.IO
 
 let parsePublicationDate (d: string) =
-    DateTimeOffset.ParseExact(d, "ddd, dd MMM yyyy HH:mm:ss zzzz", null)
+    let usCulture = CultureInfo("en-US");
+    DateTimeOffset.ParseExact(d, "ddd, dd MMM yyyy HH:mm:ss zzzz", usCulture)
 
 let toSwCategories (category: WpModel.Category []) =
     match category with
     | null -> [||]
     | _ ->
         category
-        |> Array.map (fun x ->
-            { Category.domain = x.domain
-              nicename = x.nicename
-              name = x.name })
+        |> Array.choose (fun x -> categoryMap |> Map.tryFind (x.domain, x.nicename))
+        |> Array.distinct
 
 let getItemCategories (itemMap: Map<int, WpModel.Item>) (itemId: int) =
     match itemMap |> Map.tryFind itemId with
@@ -45,9 +46,30 @@ let getWpdmFiles postmetas =
     let extracted = Regex.Replace(fileString, "a:1:{i:\d+;s:\d+:\"*([^\";]*)\"*;}", "download-manager-files/$1")
     extracted.Replace("download-manager-files//home/roedsrcr/public_html/wp//wp-content/uploads", "")
 
-let newPath (categories: SWModel.Category array) (parentCategories: SWModel.Category array) (attachedFile: string) =
-    ""
-    
+let getPrioritizedPath (categories: SWModel.CategoryMapping array) =
+    categories
+    |> Array.sortBy (fun x -> x.swCategoryPriority)
+    |> Array.map (fun x -> x.swCategoryFilePath)
+    |> Array.tryHead
+
+let newWpdmPath (categories: SWModel.CategoryMapping array) (parentCategories: SWModel.CategoryMapping array) (attachedFile: string) (year: string)=
+    let filename = Path.GetFileName(attachedFile)
+    match (getPrioritizedPath categories, getPrioritizedPath parentCategories) with
+    | Some c, _ -> (sprintf $"{c}/{filename}").Replace ("{year}", year) |> Some
+    | None, Some f -> (sprintf $"{f}/{filename}").Replace("{year}", year) |> Some
+    | _ -> None
+
+let newAttachedPath (categories: SWModel.CategoryMapping array) (parentCategories: SWModel.CategoryMapping array) (attachedFile: string) =
+    let filename = Path.GetFileName attachedFile
+    let monthPath = Path.GetDirectoryName attachedFile
+    let yearPath = Path.GetDirectoryName monthPath
+    let year = Path.GetFileName yearPath
+    match (getPrioritizedPath categories, getPrioritizedPath parentCategories) with
+    | Some c, _ -> (sprintf $"{c}/{filename}").Replace ("{year}", year) |> Some
+    | None, Some f -> (sprintf $"{f}/{filename}").Replace("{year}", year) |> Some
+    | _ -> None
+
+
 [<EntryPoint>]
 let main args =
     let rss = WpModel.deserialize @"redstavel.wordpress.2024-01-20.xml"
@@ -69,7 +91,7 @@ let main args =
             let categories = x.categories |> toSwCategories
             let parentCategories = x.post_parent |> getItemCategories
             let wpAttachedFile = x.postmetas |> getAttachedFile
-            let newPath = newPath categories parentCategories wpAttachedFile
+            let newPath = newAttachedPath categories parentCategories wpAttachedFile
 
             { Dokument.title = x.title
               publicationDate = x.pubDate |> parsePublicationDate
@@ -96,7 +118,7 @@ let main args =
             let categories = x.categories |> toSwCategories
             let parentCategories = x.post_parent |> getItemCategories
             let wpAttachedFile = x.postmetas |> getWpdmFiles
-            let newPath = newPath categories parentCategories wpAttachedFile
+            let newPath = newWpdmPath categories parentCategories wpAttachedFile (x.post_date_gmt.Substring(0, 4))
 
             { Dokument.title = x.title
               publicationDate = x.pubDate |> parsePublicationDate
@@ -104,7 +126,7 @@ let main args =
               creator = x.creator
               wpUrl = x.postmetas |> getWpdmFiles
               wpAttachedFile = wpAttachedFile
-              newPath = ""
+              newPath = newPath
               wpPostId = x.post_id
               wpParentPostId = x.post_parent
               wpPostName = x.post_name
@@ -115,21 +137,21 @@ let main args =
               tags = x.postmetas |> toSwTags
               parentTags = [||] })
 
-    // wpdmDocuments
-    // |> Array.filter (fun x -> x.title = "Årsmøtereferat 2019")
-    // |> Array.iter (fun x -> printfn $"{x}")
+    wpdmDocuments
+    |> Array.iter (fun x -> printfn $"{x.title}\t{x.wpUrl}\t{x.newPath}")
 
 // TODO: Lag ei liste basert på alle items, slik at vi kan lage ei komplett mapping
-    let usedCategories =
-        wpdmDocuments
-        |> Array.collect (fun x -> 
-            [x.categories; x.parentCategories]
-            |> Array.concat)
-        |> Array.distinct
-        |> Array.sortBy (fun x -> x.name)
-    
-    usedCategories |> Array.iter (fun x -> printfn $"{x.nicename} : {x.domain} : {x.name}")
+    // let usedCategories =
+    //     wpdmDocuments
+    //     |> Array.collect (fun x ->
+    //         [x.categories; x.parentCategories]
+    //         |> Array.concat)
+    //     |> Array.distinct
+    //     |> Array.sortBy (fun x -> x.name)
 
+    // usedCategories
+    // |> Array.iter (fun x ->
+    //     printfn $"""{{domain = "{x.domain}"; nicename = "{x.nicename}"; swCategoryName=""; swCategoryPriority=""; swCategoryFilePath=""}}; """)
 
     // rss.channel.item
     // |> Array.filter (fun x -> x.post_type = "wpdmpro")
--- wp/wp2styreweb/src/Migrate/SWModel.fs
+++ wp/wp2styreweb/src/Migrate/SWModel.fs
@@ -13,6 +13,14 @@ module SWModel =
           nicename: string
           name: string }
 
+    type CategoryMapping = {
+      domain: string
+      nicename: string
+      swCategoryName: string
+      swCategoryPriority: int
+      swCategoryFilePath: string
+    }
+
     type Dokument =
         { title: string
           publicationDate: DateTimeOffset
@@ -20,14 +28,14 @@ module SWModel =
           creator: string
           wpUrl: string
           wpAttachedFile: string
-          newPath: string
+          newPath: string option
           wpPostId: int
           wpParentPostId: int
           wpPostName: string
           wpStatus: string
           wpPostType: string
-          categories: Category []
-          parentCategories: Category []
+          categories: CategoryMapping []
+          parentCategories: CategoryMapping []
           tags: Tag []
           parentTags: Tag [] }
 
@@ -42,7 +50,48 @@ module SWModel =
           wpPostName: string
           wpStatus: string
           wpPostType: string
-          category: Category []
-          parentCategories: Category []
+          category: CategoryMapping []
+          parentCategories: CategoryMapping []
           tags: Tag []
           parentTags: Tag [] }
+
+
+    let categoryMapList = [
+      {domain = "wpdmcategory"; nicename = "formelt"; swCategoryName="Organisasjon"; swCategoryPriority=0; swCategoryFilePath="/Felles/Organisasjon"};
+      {domain = "wpdmcategory"; nicename = "infoskriv"; swCategoryName="Informasjonsskriv"; swCategoryPriority=10; swCategoryFilePath="/Felles/Informasjonsskriv"};
+      {domain = "post_tag"; nicename = "infoskriv"; swCategoryName="Informasjonsskriv"; swCategoryPriority=10; swCategoryFilePath="/Felles/Informasjonsskriv"};
+      {domain = "wpdmcategory"; nicename = "komiteer"; swCategoryName="Komite"; swCategoryPriority=100; swCategoryFilePath="/Felles/Komiteer"};
+      {domain = "post_tag"; nicename = "rsva"; swCategoryName="Vann- og avløpsprosjektet"; swCategoryPriority=5; swCategoryFilePath="/Felles/Vann- og avløpsprosjektet"};
+      {domain = "wpdmcategory"; nicename = "styremote"; swCategoryName="Styremøte"; swCategoryPriority=2; swCategoryFilePath="/Felles/Styret/{year}"};
+      {domain = "post_tag"; nicename = "styremote-referat"; swCategoryName="Styremøte"; swCategoryPriority=2; swCategoryFilePath="/Felles/Styret/{year}"};
+      {domain = "post_tag"; nicename = "styret"; swCategoryName="Styret"; swCategoryPriority=2; swCategoryFilePath="/Felles/Styret/{year}"};
+      {domain = "wpdmcategory"; nicename = "styret"; swCategoryName="Styret"; swCategoryPriority=2; swCategoryFilePath="/Felles/Styret/{year}"};
+      {domain = "wpdmcategory"; nicename = "va-anlegg"; swCategoryName="Vann- og avløpsprosjektet"; swCategoryPriority=3; swCategoryFilePath="/Felles/Vann- og avløpsprosjektet"};
+      {domain = "post_tag"; nicename = "vann-og-avlop"; swCategoryName="Vann- og avløpsprosjektet"; swCategoryPriority=3; swCategoryFilePath="/Felles/Vann- og avløpsprosjektet"};
+      {domain = "wpdmcategory"; nicename = "vannkomiteen"; swCategoryName="Vannkomiteen"; swCategoryPriority=4; swCategoryFilePath="/Felles/Komiteer/Vannkomiteen"};
+      {domain = "wpdmcategory"; nicename = "veikomiteen"; swCategoryName="Veikomiteen"; swCategoryPriority=5; swCategoryFilePath="/Felles/Komiteer/Veikomiteen"};
+      {domain = "post_tag"; nicename = "referat"; swCategoryName="Referat"; swCategoryPriority=90; swCategoryFilePath="/Felles/Styret/{year}"};
+      {domain = "post_tag"; nicename = "vei"; swCategoryName="Veikomiteen"; swCategoryPriority=5; swCategoryFilePath="/Felles/Komiteer/Veikomiteen"};
+      {domain = "wpdmcategory"; nicename = "arsmote"; swCategoryName="Årsmøter"; swCategoryPriority=80; swCategoryFilePath="/Felles/Årsmøter"};
+      {domain = "wpdmcategory"; nicename = "arsmote2010"; swCategoryName="Årsmøte 2010"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2010"};
+      {domain = "wpdmcategory"; nicename = "arsmote2011"; swCategoryName="Årsmøte 2011"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2011"};
+      {domain = "wpdmcategory"; nicename = "arsmote2012"; swCategoryName="Årsmøte 2012"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2012"};
+      {domain = "wpdmcategory"; nicename = "arsmote2013"; swCategoryName="Årsmøte 2013"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2013"};
+      {domain = "wpdmcategory"; nicename = "arsmote2014"; swCategoryName="Årsmøte 2014"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2014"};
+      {domain = "wpdmcategory"; nicename = "arsmote2015"; swCategoryName="Årsmøte 2015"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2015"};
+      {domain = "wpdmcategory"; nicename = "arsmote2016"; swCategoryName="Årsmøte 2016"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2016"};
+      {domain = "wpdmcategory"; nicename = "arsmote2017"; swCategoryName="Årsmøte 2017"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2017"};
+      {domain = "wpdmcategory"; nicename = "arsmote2018"; swCategoryName="Årsmøte 2018"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2018"};
+      {domain = "wpdmcategory"; nicename = "arsmote2019"; swCategoryName="Årsmøte 2019"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2019"};
+      {domain = "wpdmcategory"; nicename = "arsmote2020"; swCategoryName="Årsmøte 2020"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2020"};
+      {domain = "wpdmcategory"; nicename = "arsmote2021"; swCategoryName="Årsmøte 2021"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2021"};
+      {domain = "wpdmcategory"; nicename = "arsmote2022"; swCategoryName="Årsmøte 2022"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2022"};
+      {domain = "wpdmcategory"; nicename = "arsmote2023"; swCategoryName="Årsmøte 2023"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2023"};
+      {domain = "post_tag"; nicename = "arsmote-2023"; swCategoryName="Årsmøte 2023"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2023"};
+      {domain = "wpdmcategory"; nicename = "arsmote2023"; swCategoryName="Årsmøte 2023"; swCategoryPriority=1; swCategoryFilePath="/Felles/Årsmøter/Årsmøte 2023"};
+    ]
+
+    let categoryMap  =
+      categoryMapList
+      |> List.map (fun x -> ((x.domain, x.nicename), x))
+      |> Map.ofList
